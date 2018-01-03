b693827eb3c29593e5635f8fe5a5ddac26957e11
Evan Conkle <evan.conkle@gmail.com>
2018-01-03 01:16:00 -0600

    updated DB from local dev

--- db.sql
+++ db.sql
@@ -1,12 +1,30 @@
--- Create syntax for TABLE 'cache'
-CREATE TABLE `cache` (
-  `key` varchar(200) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `value` text COLLATE utf8_unicode_ci,
-  `expiration` int(11) DEFAULT NULL,
-  UNIQUE KEY `key` (`key`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.7.19)
+# Database: career_exchange 
+# Generation Time: 2018-01-03 07:13:54 +0000
+# ************************************************************
 
--- Create syntax for TABLE 'password_resets'
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table password_resets
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `password_resets`;
+
 CREATE TABLE `password_resets` (
   `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT '',
   `phone` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -14,78 +32,12 @@ CREATE TABLE `password_resets` (
   `created_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Create syntax for TABLE 'resumes'
-CREATE TABLE `resumes` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(10) unsigned NOT NULL,
-  `filename` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `active` tinyint(1) unsigned DEFAULT '0',
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `country_code` (`country_code`),
-  KEY `user_id` (`user_id`),
-  KEY `active` (`active`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Create syntax for TABLE 'sessions'
-CREATE TABLE `sessions` (
-  `id` varchar(11) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-  `payload` text COLLATE utf8_unicode_ci,
-  `last_activity` int(10) unsigned DEFAULT NULL,
-  `user_id` int(10) unsigned DEFAULT NULL,
-  `ip_address` varchar(250) COLLATE utf8_unicode_ci DEFAULT '',
-  `user_agent` text COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Create syntax for TABLE 'user_types'
-CREATE TABLE `user_types` (
-  `id` tinyint(1) unsigned NOT NULL,
-  `name` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `active` tinyint(1) unsigned DEFAULT '1',
-  PRIMARY KEY (`id`),
-  KEY `active` (`active`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- Create syntax for TABLE 'users'
-CREATE TABLE `users` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `country_code` varchar(2) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `user_type_id` int(10) unsigned DEFAULT NULL,
-  `gender_id` int(10) unsigned DEFAULT NULL,
-  `name` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `about` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `phone` varchar(60) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `phone_hidden` tinyint(1) unsigned DEFAULT '0',
-  `username` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `email` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `password` varchar(60) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `is_admin` tinyint(1) unsigned DEFAULT '0',
-  `disable_comments` tinyint(1) unsigned DEFAULT '0',
-  `receive_newsletter` tinyint(1) unsigned DEFAULT '1',
-  `receive_advice` tinyint(1) unsigned DEFAULT '1',
-  `ip_addr` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `provider` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `provider_id` int(10) unsigned DEFAULT NULL,
-  `email_token` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `phone_token` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `verified_email` tinyint(1) unsigned DEFAULT '1',
-  `verified_phone` tinyint(1) unsigned DEFAULT '0',
-  `blocked` tinyint(1) unsigned DEFAULT '0',
-  `closed` tinyint(1) unsigned DEFAULT '0',
-  `last_login_at` datetime DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `deleted_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `country_code` (`country_code`),
-  KEY `user_type_id` (`user_type_id`),
-  KEY `gender_id` (`gender_id`),
-  KEY `phone` (`phone`),
-  KEY `username` (`username`),
-  KEY `email` (`email`),
-  KEY `verified_email` (`verified_email`),
-  KEY `verified_phone` (`verified_phone`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
